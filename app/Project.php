737186d51c6c8eb3aac8ecf7db67478d737f642f
Jacob Landry <jacoblandry@Jacobs-Air.hsd1.ca.comcast.net>
2017-10-09 22:09:11 -0700

    adding comments to project class

--- app/Project.php
+++ app/Project.php
@@ -9,33 +9,63 @@ class Project extends Model
     protected $guarded = ['id'];
     protected $dates = ['created_at', 'updated_at'];
 
+    /**
+     * Get tasks applied to this project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function tasks()
     {
         return $this->hasMany('App\Task');
     }
 
+    /**
+     * Get tasks logged as bugs against this project
+     *
+     * @return $this
+     */
     public function bugs()
     {
         return $this->hasMany('App\Task')->where('type', 'bug');
     }
 
+    /**
+     * Get tasks logged as new features against this project
+     *
+     * @return $this
+     */
     public function features()
     {
         return $this->hasMany('App\Task')->where('type', 'feature');
     }
 
+    /**
+     * Get users attached to this project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function users()
     {
         return $this->belongsToMany('App\User');
     }
 
+    /**
+     * Get releases attached to this project
+     *
+     * @return mixed
+     */
     public function releases()
     {
         return $this->hasMany('App\Release')->withTrashed();
     }
 
+    /**
+     * Get comments attached to this project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function comments()
     {
-        return $this->hasMany('App\ProjectComment');
+        return $this->hasMany('App\ProjectComment')->orderBy('created_at', 'asc');
     }
 }
