ea50114ae035e4a106f793a959d5ab52ceb8063d
Jacob Landry <landry.jacob@gmail.com>
2023-02-27 22:50:18 -0500

    init

---
+++ database/migrations/2023_02_28_031610_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            // identifiers
+            $table->$table->bigIncrements('id')->index();
+            $table->$table->bigInteger('parent_id')->nullable()->index();
+            // data
+            $table->string('title');
+            $table->longtext('description')->nullable();
+            $table->string('status', 10)->index();
+            // flags
+            $table->boolean('is_complete')->default('false');
+            $table->boolean('hide_when_complete')->default('true');
+            // timestamps
+            $table->timestamps();
+            $table->$table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
