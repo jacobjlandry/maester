182db7df08d1613b4b73402ac0ebe338fc954ac0
Jacob Landry <landry.jacob@gmail.com>
2023-05-25 23:08:18 -0400

    basic modal

--- resources/js/components/tasks/Create.vue
+++ resources/js/components/tasks/Create.vue
@@ -1,18 +1,25 @@
 <template>
     <div v-if="showing">
-        The modal will go here.
+        <div class="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-1/2 h-2/3 bg-white border rounded-lg">
+            <div class="create-task-header w-full h-12 bg-gray-500 px-4 py-2 flex justify-between text-white">
+                Create Task
+                <button
+                    class="bg-blue-600 text-white px-4 py-2 text-sm uppercase tracking-wide font-bold rounded-lg"
+                    @click="this.close()"
+                    >
+                    X
+                </button>
+            </div>
+            <div class="create-task-body p-4">
+                Create Task Form
+            </div>
+        </div>        
     </div>
     <button
         class="bg-blue-600 text-white px-4 py-2 text-sm uppercase tracking-wide font-bold rounded-lg"
         @click="this.open()"
         >
-        Show Modal
-    </button>
-    <button
-        class="bg-blue-600 text-white px-4 py-2 text-sm uppercase tracking-wide font-bold rounded-lg"
-        @click="this.close()"
-        >
-        Close Modal
+        New Task
     </button>
 </template>
 
