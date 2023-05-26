d94659f11e3970ef1c99d9b05445d386c1e2e5d4
Jacob Landry <landry.jacob@gmail.com>
2023-05-25 23:11:02 -0400

    simple transition

--- resources/js/components/tasks/Create.vue
+++ resources/js/components/tasks/Create.vue
@@ -1,20 +1,22 @@
 <template>
-    <div v-if="showing">
-        <div class="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-1/2 h-2/3 bg-white border rounded-lg">
-            <div class="create-task-header w-full h-12 bg-gray-500 px-4 py-2 flex justify-between text-white">
-                Create Task
-                <button
-                    class="bg-blue-600 text-white px-4 py-2 text-sm uppercase tracking-wide font-bold rounded-lg"
-                    @click="this.close()"
-                    >
-                    X
-                </button>
-            </div>
-            <div class="create-task-body p-4">
-                Create Task Form
-            </div>
-        </div>        
-    </div>
+    <Transition name="fade">
+        <div v-if="showing">
+            <div class="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-1/2 h-2/3 bg-white border rounded-lg">
+                <div class="create-task-header w-full h-12 bg-gray-500 px-4 py-2 flex justify-between text-white">
+                    Create Task
+                    <button
+                        class="bg-blue-600 text-white px-4 py-2 text-sm uppercase tracking-wide font-bold rounded-lg"
+                        @click="this.close()"
+                        >
+                        X
+                    </button>
+                </div>
+                <div class="create-task-body p-4">
+                    Create Task Form
+                </div>
+            </div>        
+        </div>
+    </Transition>
     <button
         class="bg-blue-600 text-white px-4 py-2 text-sm uppercase tracking-wide font-bold rounded-lg"
         @click="this.open()"
@@ -40,3 +42,14 @@
         }
     }
 </script>
+
+<style scoped>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: all 0.4s;
+    }
+    .fade-enter,
+        .fade-leave-to {
+        opacity: 0;
+    }
+</style>
